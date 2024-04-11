dae3a101e6eb848d1b16de66a3f11d236771e68d
Maria Eduarda L. Alencar <160172189+alencarduda@users.noreply.github.com>
2024-04-11 08:07:46 -0300

    Add files via upload

---
+++ Sistemas Operacionais/Arquivo BAT/SO - Aba Encerrar Arquivos.bat
@@ -0,0 +1,3 @@
+START /IM notepad.exe
+START IM calc.exe
+START /IM mspaint.exe
---
+++ Sistemas Operacionais/Arquivo BAT/SO - Aba Menu dos Programas.bat
@@ -0,0 +1,4 @@
+echo Escolha uma opção
+echo 1 - abrir Calculadora
+echo 2 - abrir Bloco de Notas
+echo 3 - abrir Paint
